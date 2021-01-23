e83e034ab44f833187cd236670a648e985d3c667
Michael Farber Brodsky <taco@mailo.com>
2021-01-23 15:02:31 +0200

    Add addition

--- BigInt.c
+++ BigInt.c
@@ -1,4 +1,7 @@
 #include "BigInt.h"
+#ifndef max
+#define max(a,b) (((a) > (b)) ? a : b)
+#endif
 
 void BigInt_free(BigInt x) {
   free(x.digits);
@@ -6,8 +9,8 @@ void BigInt_free(BigInt x) {
 
 BigInt BigInt_clone(BigInt x) {
   BigInt y;
-  y.len = x.len;
   y.sign = x.sign;
+  y.len = x.len;
   y.digits = malloc(x.len * sizeof(u32));
   memcpy(y.digits, x.digits, x.len * sizeof(u32));
   return y;
@@ -27,7 +30,7 @@ char *BigInt_to_binary_string(BigInt x) {
         *(current_char++) = '1';
         found_nonzero_digit = true;
       } else if (found_nonzero_digit) {
-        // Don't show trailing zeros
+        // don't show trailing zeros
         *(current_char++) = '0';
       }
     }
@@ -38,4 +41,68 @@ char *BigInt_to_binary_string(BigInt x) {
   }
   *current_char = '\0';
   return result;
+}
+
+BigInt BigInt_add(BigInt x, BigInt y) {
+  // if one of them is negative and the other isn't, this is subtraction
+  if (y.sign && !x.sign) {
+    return BigInt_sub(y, x);
+  } else if (x.sign && !y.sign) {
+    return BigInt_sub(x, y);
+  }
+  // make sure x.len >= y.len
+  if (y.len > x.len) {
+    return BigInt_add(y, x);
+  }
+  BigInt z;
+  z.len = x.len;
+  z.sign = x.sign; // equal to y's sign
+  z.digits = malloc(z.len * sizeof(u32));
+  memset(z.digits, 0, z.len * sizeof(u32));
+  bool carry = false;
+  for (ssize_t i = x.len - 1; i >= 0; --i) {
+    u32 a = x.digits[x.len - i - 1];
+    if (i < y.len) {
+      u32 b = y.digits[x.len - i - 1];
+      for (u32 current_digit = 1; current_digit != 1 << 31; current_digit <<= 1) {
+        u32 digit_a = a & current_digit;
+        u32 digit_b = b & current_digit;
+        bool both = digit_a && digit_b;
+        bool only_one = (digit_a && !digit_b) || (digit_b && !digit_a);
+        if (carry) {
+          if (both) {
+            z.digits[z.len - i - 1] |= current_digit;
+            carry = false;
+          } else if (only_one) {
+            carry = true;
+          } else {
+            z.digits[z.len - i - 1] |= current_digit;
+            carry = false;
+          }
+        } else {
+          if (both) {
+            carry = true;
+          } else if (only_one) {
+            z.digits[z.len - i - 1] |= current_digit;
+          }
+        }
+      }
+    } else {
+      for (u32 current_digit = 1; current_digit != 1 << 31; current_digit <<= 1) {
+        u32 digit_a = a & current_digit;
+        if (carry) {
+          if (!digit_a) {
+            z.digits[z.len - i - 1] |= current_digit;
+          }
+        } else if (digit_a) {
+          z.digits[z.len - i - 1] |= current_digit;
+        }
+      }
+    }
+  }
+  return z;
+}
+
+BigInt BigInt_sub(BigInt x, BigInt y) {
+  return x;
 }
--- BigInt.h
+++ BigInt.h
@@ -6,11 +6,13 @@
 typedef u_int32_t u32;
 
 struct {
-  ssize_t len;
   bool sign;
+  ssize_t len;
   u32* digits; // 2**32 would be [1,0], 2**64 would be [1,0,0]
 } typedef BigInt;
 
 void BigInt_free(BigInt);
 BigInt BigInt_clone(BigInt);
 char *BigInt_to_binary_string(BigInt);
+BigInt BigInt_add(BigInt x, BigInt y);
+BigInt BigInt_sub(BigInt x, BigInt y);
